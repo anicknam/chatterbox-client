59a7ed1d35896e233514107fd10821c689ee9b22
anicknam <a.nicknam@gmail.com>
2016-09-26 16:55:51 -0700

    First attempt. Passed tests up to events

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,89 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.init = function() {};
+
+app.send = function(message) {
+  // var message = {
+  //   username: window.location.search.replace('?username=', ''),
+  //   text: $('.message').value,
+  //   roomname: '4chan'
+  // };
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+
+};
+
+app.fetch = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+      for ( var i = 0; i < data['results'].length; i++) {
+        console.log(data['results'][i].text);
+        app.renderMessage(data['results'][i]);
+      }
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.server = 'https://api.parse.com/1/classes/messages';
+
+app.clearMessages = function() {
+  $('#chats').children().remove();
+};
+
+app.renderMessage = function(message) {
+
+  $('#chats').append('<div>' + message['text'] + '</div>');
+
+};
+
+app.renderRoom = function(roomName) {
+  $('#roomSelect').append('<div>' + roomName + '</div>');
+};
+
+app.handleUsernameClick = function () {
+
+};
+
+app.fetch();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
