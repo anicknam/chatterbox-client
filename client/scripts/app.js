5e8e0dc3cd7dae9d64ed83c374e8ca534cccdb63
anicknam <a.nicknam@gmail.com>
2016-09-26 20:59:44 -0700

    End of day 1

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,85 +1,100 @@
-// YOUR CODE HERE:
-var app = {};
 
-app.init = function() {};
 
-app.send = function(message) {
-  // var message = {
-  //   username: window.location.search.replace('?username=', ''),
-  //   text: $('.message').value,
-  //   roomname: '4chan'
-  // };
+  // YOUR CODE HERE:
+  var app = {};
 
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/messages',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
+  app.init = function() {
 
-};
-
-app.fetch = function() {
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/messages',
-    type: 'GET',
-    contentType: 'application/json',
-    success: function (data) {
-      console.log(data);
-      for ( var i = 0; i < data['results'].length; i++) {
-        //console.log(data['results'][i].text);
-        app.renderMessage(data['results'][i]);
-      }
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
-};
-
-app.server = 'https://api.parse.com/1/classes/messages';
-
-app.clearMessages = function() {
-  $('#chats').children().remove();
-};
-
-app.renderMessage = function(message) {
-  
-  var $div = $('<div></div>');
-  $div.addClass(message.objectId);
-  var func = function(type) {
-    var $newDOM = $('<span></span><br>');
-    $newDOM.addClass(type);
-    $($newDOM).text((type === 'username') ? '@' + message[type] : message[type]);
-    $($div).append($newDOM);
   };
 
-  func('username');
-  func('text');
-  $('#chats').append($div);
-  
+  app.send = function(message) {
+    // var message = {
+    //   username: window.location.search.replace('?username=', ''),
+    //   text: $('.message').value,
+    //   roomname: '4chan'
+    // };
 
-};
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
 
-app.renderRoom = function(roomName) {
-  $('#roomSelect').append('<div>' + roomName + '</div>');
-};
+  };
 
-app.handleUsernameClick = function () {
+  app.fetch = function() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data);
+        for ( var i = 0; i < data['results'].length; i++) {
+          //console.log(data['results'][i].text);
+          app.renderMessage(data['results'][i]);
+        }
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      }
+    });
+  };
 
-};
+  app.server = 'https://api.parse.com/1/classes/messages';
 
-app.fetch();
+  app.clearMessages = function() {
+    $('#chats').children().remove();
+  };
+
+  app.renderMessage = function(message) {
+    
+    var $div = $('<div></div>');
+    $div.addClass(message.objectId);
+    var func = function(type) {
+      if (type === 'username') {
+        var $newDOM = $('<span></span><br>');
+      } else {
+        var $newDOM = $('<span></span><br>');
+      }
+      $newDOM.addClass(type);
+      $($newDOM).text((type === 'username') ? '@' + message[type] : message[type]);
+      $($div).append($newDOM);
+    };
+
+    func('username');
+    func('text');
+    $('#chats').append($div);
+    
+
+  };
+
+  app.renderRoom = function(roomName) {
+    $('#roomSelect').append('<div>' + roomName + '</div>');
+  };
+
+  app.handleUsernameClick = function () {
+    window.location['friends'] = window.location['friends'] || {};
+    window.location['friends'][this.innerHTML.replace('@', '')] = this.innerHTML.replace('@', '');
+  };
+
+  $(document).ready(function() {
+    $('body').on('click', '.username', function() {
+      console.log('hello');
+      app.handleUsernameClick();
+    });    
+  });
+
+  app.fetch();
 
 
 
